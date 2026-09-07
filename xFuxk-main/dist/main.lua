213c515a968d9ba3ea452c2f0f5253855c44104c
Txzp <tu_email@ejemplo.com>
2026-09-07 14:58:15 -0400

    fix: restore slider and update color

--- xFuxk-main/dist/main.lua
+++ xFuxk-main/dist/main.lua
@@ -6498,22 +6498,31 @@ ParentConfig=ak,
 
 
 
+
+al.UIElements.SliderIcon=ae.NewRoundFrame(99,"Squircle",{
+ImageTransparency=.95,
+Size=UDim2.new(1,not al.IsTextbox and-aw or(-al.TextBoxWidth-8),0,4),
+AnchorPoint=Vector2.new(0.5,0.5),
+Position=UDim2.new(0.5,0,0.5,0),
+Name="Frame",
+ThemeTag={
+ImageColor3="Text",
+},
+},{
+
 ae.NewRoundFrame(99,"Squircle",{
 Name="Frame",
 Size=UDim2.new(ar,0,1,0),
 ImageTransparency=.1,
-ImageColor3=Color3.fromHex"#217DC3",
+
+ImageColor3=Color3.fromRGB(0,255,106),
 },{
 
 ae.NewRoundFrame(99,"Squircle",{
-Size=UDim2.new(
-0,
-ak.Window.NewElements and(al.ThumbSize*2)or(al.ThumbSize+2),
-0,
-ak.Window.NewElements and(al.ThumbSize+4)or(al.ThumbSize+2)
-),
+Size=UDim2.new(0,ak.Window.NewElements and(al.ThumbSize*2)or(al.ThumbSize+2),0,ak.Window.NewElements and(al.ThumbSize+4)or(al.ThumbSize+2)),
 Position=UDim2.new(1,0,0.5,0),
 AnchorPoint=Vector2.new(0.5,0.5),
+
 ImageColor3=Color3.fromRGB(255,255,255),
 Name="Thumb",
 },{
@@ -6523,7 +6532,8 @@ ImageColor3=Color3.new(1,1,1),
 Name="Highlight",
 ImageTransparency=.6,
 }),
-}),
+})
+})
 })
 
 
