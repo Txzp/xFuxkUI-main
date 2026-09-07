92d904f18f39d5e42d8605da6c77c314e598cb60
Txzp <tu_email@ejemplo.com>
2026-09-06 20:53:13 -0400

    feat: theme changes

--- xFuxk-main/src/themes/Init.lua
+++ xFuxk-main/src/themes/Init.lua
@@ -3,11 +3,11 @@ return function(WindUI, Creator)
 		Dark = {
 			Name = "Dark",
 			Accent = Color3.fromHex("#18181b"),
-			Dialog = Color3.fromHex("#0F0F0F"),
+			Dialog = Color3.fromHex("#404040"),
 			Outline = Color3.fromHex("#FFFFFF"),
 			Text = Color3.fromHex("#FFFFFF"),
 			Placeholder = Color3.fromHex("#7a7a7a"),
-			Background = Color3.fromHex("#050505"),
+			Background = Color3.fromHex("#000000"),
 			Button = Color3.fromHex("#52525b"),
 			Icon = Color3.fromHex("#a1a1aa"),
 			Toggle = Color3.fromHex("#33C759"),
